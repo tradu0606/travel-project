322359ca3975b2adfa2aaf67a3e8d7eb3d1c101c
Tetyana Radu <tanyaradu@Tanyas-MacBook-Pro.local>
2019-06-17 19:10:38 -0400

    connection changes

--- connections.js
+++ connections.js
@@ -1,13 +1,19 @@
 const mongoose =require("mongoose")
 mongoose.Promise = Promise
-let mongoURI = ''
+
 
   
   if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL;
+    mongoose.connect(process.env.DB_URL);
   } else {
-    mongoURI = "mongodb://localhost/travel";
+    mongoose
+      .connect('mongodb://localhost/travel"', { useNewUrlParser: true })
+      .then((conn) => {
+        console.log(`connected to mongodb on ${conn.connections[0].name} db`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
-mongoose.connect('mongodb://localhost/travel', {useNewUrlParser: false})
-module.exports = mongoose
 
+module.exports = mongoose
